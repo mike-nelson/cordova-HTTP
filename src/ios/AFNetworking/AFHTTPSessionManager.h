1414da53052d29d675773a505d5d11f784cb0b27
Mike Nelson <mike@beweb.co.nz>
2015-09-11 16:50:33 +1200

    OPTIONS

--- src/ios/AFNetworking/AFHTTPSessionManager.h
+++ src/ios/AFNetworking/AFHTTPSessionManager.h
@@ -233,6 +233,14 @@
                          success:(void (^)(NSURLSessionDataTask *task, id responseObject))success
                          failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure;
 
+
+ */
+ 
+- (NSURLSessionDataTask *)OPTIONS:(NSString *)URLString
+                      parameters:(id)parameters
+                         success:(void (^)(NSURLSessionDataTask *task, id responseObject))success
+                         failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure;
+                         
 @end
 
 #endif
